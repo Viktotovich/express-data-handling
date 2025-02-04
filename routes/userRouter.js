d5915f7e981f0b9392bf1e13373506c20cb3a7e9
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-02-05 00:03:22 +0400

    Created the update user controls and partial/page

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,4 +6,7 @@ userRouter.get("/", usersController.usersListGet);
 userRouter.get("/create", usersController.usersCreateGet);
 userRouter.post("/create", usersController.usersCreatePost);
 
+userRouter.get("/:id/update", usersController.usersUpdateGet);
+userRouter.post("/:id/update", usersController.usersUpdatePost);
+
 module.exports = userRouter;
